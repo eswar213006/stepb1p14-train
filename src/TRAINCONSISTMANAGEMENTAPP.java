54fa05e034eec437d40185e167226977da78e7ec
eswar213006 <eswartunuguntla@gmail.com>
2026-04-08 14:45:15 +0530

    uc1

--- src/TRAINCONSISTMANAGEMENTAPP.java
+++ src/TRAINCONSISTMANAGEMENTAPP.java
@@ -5,33 +5,16 @@ public class TRAINCONSISTMANAGEMENTAPP{
 
     public static void main(String[] args) {
 
+
         System.out.println("=== Train Consist Management App ===");
 
 
-        List<String> passengerBogies = new ArrayList<>();
+        List<String> bogies = new ArrayList<>();
 
 
-        passengerBogies.add("Sleeper");
-        passengerBogies.add("AC Chair");
-        passengerBogies.add("First Class");
+        System.out.println("Train initialized successfully.");
+        System.out.println("Current number of bogies: " + bogies.size());
 
 
-        System.out.println("\nPassenger Bogies after adding:");
-        System.out.println(passengerBogies);
-
-
-        passengerBogies.remove("AC Chair");
-
-
-        System.out.println("\nPassenger Bogies after removal:");
-        System.out.println(passengerBogies);
-
-
-        boolean exists = passengerBogies.contains("Sleeper");
-        System.out.println("\nDoes Sleeper bogie exist? " + exists);
-
-
-        System.out.println("\nFinal Passenger Bogie List:");
-        System.out.println(passengerBogies);
     }
 }
