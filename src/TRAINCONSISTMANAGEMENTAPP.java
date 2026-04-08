37bd46a711c6b2905f447fea9fff5d9c24e1b30c
eswar213006 <eswartunuguntla@gmail.com>
2026-04-08 14:25:50 +0530

    u1

---
+++ src/TRAINCONSISTMANAGEMENTAPP.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TRAINCONSISTMANAGEMENTAPP {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("=== Train Consist Management App ===");
+
+        List<String> bogies = new ArrayList<>();
+
+
+        System.out.println("Train initialized successfully.");
+        System.out.println("Current number of bogies: " + bogies.size());
+
+
+    }
+}
